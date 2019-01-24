1508eae41e5170c5aa31cbba64a02accdd4ae6bd
CatherineVSh <mail2shev@gmail.com>
2019-01-24 15:57:08 +0300

    l01 hard01 added

---
+++ hard01.py
@@ -0,0 +1,8 @@
+print('Welcome to Physical Shape evaluation tool')
+name = input('What is your name?')
+lastname = input('What is your last name?')
+print('Hello, ', name, lastname'. Please provide your data to start evaluation')
+age = int(input('How old are you?'))
+weight = int(input('How much do you weight?'))
+if age<=30 and (weight>=50 and weight<=120):
+    print('You are in a good physical shape')
